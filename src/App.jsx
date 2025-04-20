6c06872fe8988aae576c9662c1354f75be661fb1
Shreya <shreya.saranvj@gmail.com>
2025-04-20 23:43:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Hero from './components/hero.jsx'
+import About from "./components/About";
+import Navbar from './components/Navbar.jsx';
+import Features from './components/Features.jsx'
+import Story from './components/Story.jsx';
+import Contact from './components/Contact.jsx';
+import Footer from './components/Footer.jsx';
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      {/* <Story /> */}
+      <Contact />
+      <Footer />
+
+      
+      
+
+    </main>
+  )
+}
+
+export default App
